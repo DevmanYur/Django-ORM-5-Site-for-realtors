92e29476935d1a85e8eb6fbbad0bce08e2804905
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2023-11-30 16:25:40 +0300

    Идемпотентная миграция : Перенос данных о владельцах в модель Owner

--- property/admin.py
+++ property/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from .models import Flat, Claim, Owner
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'town_district', 'address',)
+    search_fields = ('owner', 'owners_phonenumber',  'town', 'town_district', 'address',)
     readonly_fields = ['created_at']
-    list_display = ('address', 'price','new_building','construction_year','town',)
+    list_display = ('owner', 'owners_phonenumber','address', 'price','new_building','construction_year','town',)
     list_editable = ['new_building']
     list_filter = ('new_building','rooms_number','has_balcony',)
     raw_id_fields = ('liked_by',)
