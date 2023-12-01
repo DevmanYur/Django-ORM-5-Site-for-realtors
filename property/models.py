612762dd20ea0959914aac98aeea9e9456c0151f
DevmanYur <142353662+DevmanYur@users.noreply.github.com>
2023-12-01 13:36:55 +0300

    Mig empty

--- property/models.py
+++ property/models.py
@@ -70,10 +70,10 @@ class Claim(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField('ФИО владельца', max_length=200)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    owner_pure_phone = PhoneNumberField(verbose_name="Нормализованный номер владельца", blank=True)
-    owner_flat = models.ManyToManyField(Flat,verbose_name="Квартиры в собственности", related_name="owners")
+    owner = models.CharField('ФИО владельца', max_length=200,db_index=True)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20,db_index=True)
+    owner_pure_phone = PhoneNumberField(verbose_name="Нормализованный номер владельца", blank=True,db_index=True)
+    owner_flat = models.ManyToManyField(Flat,verbose_name="Квартиры в собственности", related_name="owners",db_index=True)
 
 
 # Owner
